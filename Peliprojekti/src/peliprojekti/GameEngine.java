7f5f8625e6f4b4f9af13fe650189efeb5ce82883
juliusniiniranta <juliusniiniranta@me.com>
2015-03-31 11:21:02 +0300

    testi2

--- Peliprojekti/src/peliprojekti/GameEngine.java
+++ Peliprojekti/src/peliprojekti/GameEngine.java
@@ -1,11 +1,36 @@
 package peliprojekti;
 
 public class GameEngine {
+	private Player player;
+	private CommandInterpreter newCommandInterpreter = new CommandInterpreter(); 
+	private Calendar newCalendar = new Calendar();
 
-	public void gameLoop() {
-		// Pelilooppi
-		// Alkuvalikko
-		
+	public GameEngine(Player player) {
+		this.player = player;
 	}
 
+	public void gameLoop() {
+		boolean quit = false;
+		System.out.println("KelaSim");
+		while (quit == false) { // Tässä loopissa pelaaminen tapahtuu. 
+			System.out.println("Day "+newCalendar.getDateCounter());
+			String command = newCommandInterpreter.consoleCommand();
+			System.out.println("Valitsit: " + command);
+			switch (command) {
+			case "q":
+				quit = true;
+				break;
+			case "testi":
+				System.out.println("Testi toimii. ");
+				break;
+			case "stats":
+				System.out.println("Raha: "+player.getMoney()+", terveys: "+player.getHealth());
+				break;
+			case "skip":
+				newCalendar.increaseDateCounter();
+				break;
+			}
+		}
+		System.out.println("Exit gameloop");
+	}
 }
