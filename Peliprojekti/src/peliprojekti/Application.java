952e08cbefc8fceb21506e93a0f1ffc4742a8df9
jasusetala <jasu.setala@metropolia.fi>
2015-04-21 11:39:00 +0300

    Application

---
+++ Peliprojekti/src/peliprojekti/Application.java
@@ -0,0 +1,32 @@
+package peliprojekti;
+
+public class Application {
+
+		private Calendar newCalendar = new Calendar();
+		private int kasittelypaiva = 0;
+		private int x = 0;
+
+		public void fillApplication() {
+			// lomakkeen täyttäminen
+			x = (int) (Math.random()*20) + 10; // testausta varten
+			kasittelypaiva = newCalendar.getDateCounter() + x; // testausta varten
+			System.out.println("Lomake on täytetty, odota "+x+" päivää.");
+		}
+
+		/* public int getKasittelypaiva() {
+			return kasittelypaiva;
+		} */
+
+		public void checkApplication() {
+		// tehdään GameEnginen while looppissa
+			if (kasittelypaiva == newCalendar.getDateCounter()) {
+				// tuet tulee
+				Player.money = Player.money + 100;
+				System.out.println("Hakemuksesi on käsitelty!");
+				System.out.println("Raha: +100");
+			} else {
+				// tuet eivät tule
+		}
+	}
+
+}
